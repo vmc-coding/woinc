19c6801dd94db7369b9d2967915d110c6da9f470
vmc <vmc.coding@gmail.com>
2021-07-18 01:46:54 +0200

    qt: Add copyright to test

--- ui/qt/tests/tab_model_updater_test.cc
+++ ui/qt/tests/tab_model_updater_test.cc
@@ -1,3 +1,21 @@
+/* ui/qt/tests/tab_model_updater_test.cc --
+   Written and Copyright (C) 2018-2021 by vmc.
+
+   This file is part of woinc.
+
+   woinc is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   woinc is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with woinc. If not, see <http://www.gnu.org/licenses/>. */
+
 #include <cassert>
 #include <iostream>
 #include <vector>
