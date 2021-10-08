09419a0374692b91eb6e8c70f249bf38052e7975
vmc <vmc.coding@gmail.com>
2021-10-08 21:26:52 +0200

    Add shortcut for retrying transfers

--- ui/qt/menu.cc
+++ ui/qt/menu.cc
@@ -204,7 +204,7 @@ ToolsMenu::ToolsMenu(const QString &title, QWidget *parent)
     connect(addAction("Run CPU &benchmarks"), &QAction::triggered, [&]() {
         emit cpu_benchmarks_to_be_run(selected_host_);
     });
-    connect(addAction("Retry pending transfers"), &QAction::triggered, [&]() {
+    connect(addAction("Retry pending &transfers"), &QAction::triggered, [&]() {
         emit pending_transfers_to_be_retried(selected_host_);
     });
 }
